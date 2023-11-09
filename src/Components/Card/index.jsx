91a33b6465ab302ba14e91005411f80fde04045d
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-11-08 22:20:46 -0500

    useefect info-card-api

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,29 @@
+function Card({data}) {
+  return(
+    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+      <figure className="relative mb-5 w-full h-4/5">
+        <span 
+          className="absolute text-black text-sm bottom-0 left-0 bg-black/20 rounded-lg m-2 px-3 py-0.5"
+        >
+          {data.category.name}
+        </span>
+        <img 
+          className="w-full h-full object-cover rounded-lg" 
+          src={data.images[0]} 
+          alt={data.title} />
+        <div 
+             className="absolute top-0 right-0 flex justify-center items-center bg-black/10 w-6 h-6 rounded-full text-black m-2 p-1"
+        >
+          +
+        </div>
+      </figure>
+      <p className="flex justify-between">
+        <span className="text-black text-sm font-light">{data.title}</span>
+        <span className="text-black text-lg font-medium">${data.price}.00</span>
+      </p>
+
+    </div>   
+  );
+}
+
+export {Card};
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,11 +1,26 @@
 import { Layout } from "../Components/Layout";
-import { Card } from "../Components/card";
+import { Card } from "../Components/Card";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect( () => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then(response => response.json())
+      .then(dataFetch => setItems(dataFetch))
+  }, []);
+  
   return(
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item}/>
+          ))
+        }
+      </div>
     </Layout>
   );
 }
